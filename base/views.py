35589f62beea40542f08719030146b2a8fb3368c
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-02-21 18:23:28 -0500

    Add a home page

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def HomeView(request, *args, **kwargs):
+    template = "home.html"
+    context = {
+
+    }
+
+    return render(request, template, context)
