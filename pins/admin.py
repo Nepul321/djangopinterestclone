4654ec0a3f492b027e9a611bfbd33704e433cd8f
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2023-02-22 19:51:46 -0500

    Create database model for image pins

--- pins/admin.py
+++ pins/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ImagePin, PinView
 
-# Register your models here.
+admin.site.register(ImagePin)
+admin.site.register(PinView)
--- pins/models.py
+++ pins/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class PinView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-datetime' , ]
+
+class ImagePin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="media/posts/images/%Y/%m/%d/%H/%M")
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    is_private = models.BooleanField(default=False)
+    datetime = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="video_post_likes", blank=True)
+    dislikes = models.ManyToManyField(User, related_name="video_posts_dislikes", blank=True)
+    views = models.ManyToManyField(PinView, related_name="video_post_views", blank=True)
+    view_count = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-datetime', ]
+
+    def set_view_count(self):
+        views = self.views.count()
+        self.view_count = views
+        self.save()
